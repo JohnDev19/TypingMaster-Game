fd80b1ef1fb39a0807aa4c5271ee02208e36a4dc
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2025-06-04 21:12:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,16 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/bgmsc",
+        source: "/bgmsc/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/images/:path*",
         headers: [
           {
             key: "Cache-Control",
